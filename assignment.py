98becf2de3e7a617858c90cdb56c10169b4ae003
indr007 <90273802+indr007@users.noreply.github.com>
2021-09-07 23:13:11 +0530

    Assignment 01 Submitted

---
+++ assignment.py
@@ -0,0 +1,32 @@
+# Qustion 01
+
+# multiplication
+a=10
+b=20
+print(a*b)
+
+#devide
+a=10
+b=20
+print(a/b)
+
+# modulation
+a=10
+b=20
+print(a%b)
+
+#bodmass
+a=10
+b=20
+print((a*b)+(a/b))
+
+# Qustion 02
+
+# simple interest
+
+P=1
+T=1
+R=1
+
+simple_interest = (P*T*R)/100
+print("simple interest is :", simple_interest)
